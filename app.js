c5246df29eaf9f867a2445e3a2bce9feb253d904
Chinh Do <do.inby@gmail.com>
2021-09-09 09:20:20 +1000

    change background color according to result

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import getUsersList from "./utils/usersList.js";
 
 class App {
     constructor() {
+        this.$body = document.querySelector('body');
         this.$usernameInput = document.querySelector('#username-input');
         this.$usernamesData = document.querySelector('#usernames-data');
         this.$errorMessage = document.querySelector('#error-message');
@@ -9,35 +10,47 @@ class App {
         this.usersList = getUsersList();
         this.isMatched;
         this.match = '';
-        
+
         this.getInputData();
     }
     
     getInputData() {
         this.$usernameInput.addEventListener('keyup', (event) => {
-            this.isMatched = false;
             const hasSearchQuery = this.$usernameInput.value.length > 0;
+
+            // If search bar has search key, show result
             if(hasSearchQuery) {
+                this.isMatched = false;
                 const searchQuery = event.target.value.toLowerCase();
-                console.log('searchQuery:', searchQuery)
                 
                 for(let i in this.usersList) {
-                    if(searchQuery === this.usersList[i].toLowerCase()) {
+                    const currentUsername = this.usersList[i];
+
+                    // Comparing seach key to database
+                    if(searchQuery === currentUsername.toLowerCase()) {
                         this.isMatched = true;
-                        this.match = this.usersList[i];
+                        this.match = currentUsername;
                     }
                 }
                 this.render();
             }
         });
     }
-    
+
     render() {
+        // If match found, show matched username
         if (this.isMatched) {
             this.$errorMessage.style.display = "none";
-          this.$usernamesData.innerHTML = `<li class="username">${this.match}</li>`;
-        } else {
+            this.$body.classList.remove("error");
+            this.$body.classList.add('success');
+            this.$usernamesData.innerHTML = `<li class="username">${this.match}</li>`;
+        } 
+
+        // If not, clear search result and show error message
+        else if(this.isMatched = 'false') {
             this.$errorMessage.style.display = 'block';
+            this.$body.classList.remove("success");
+            this.$body.classList.add("error");
             this.$usernamesData.innerHTML = '';
         }
     }
